42d3d296e752866008eb1043272288ad9a74e74a
Dolgov Oleg <83357001+Fatality6@users.noreply.github.com>
2023-03-06 12:56:10 +0300

    info, filter, antd
    Добавил модальное окно для вывода информации о других пользователях, а так же добавил фильтрацию отображения пользователей для исключения отображения авторизованного пользователя в списке.
    Удалена зависимость  antd

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom'
 import noPhoto from '../common/img/user.jpg'
 
-const UserItem = ({ username, userPhoto }) => {
-  return <Link to={`/people`}>
-    <div className='flex items-center flex-col p-2 rounded-md'>
+const UserItem = ({ username, userPhoto, birthdate, setIsOpen, setActiveName, setActiveBirthdate, setActiveImage }) => {
+  return (
+    <div 
+      className='flex items-center flex-col rounded-md cursor-pointer w-[250px] h-[300px]' 
+      onClick={()=>{
+        setIsOpen(true)
+        setActiveName(username)
+        setActiveBirthdate(birthdate)
+        setActiveImage(userPhoto)}}>
       <img 
         alt={username}
-        className='w-25 rounded-[50%]'
+        className='w-25 rounded-[50%] p-1 hover:shadow-xl hover:bg-white duration-300'
         src={userPhoto?`http://localhost:8080/${userPhoto}`:noPhoto}/>
-      <span>{username}</span>
-    </div>
-  </Link>
+      <span className='mt-4'>{username}</span>
+    </div>)
 }
 export default UserItem
--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import Info from '../components/Info'
 import Navbar from '../components/Navbar'
 import UserItem from '../components/UserItem'
 import { checkIsAuth } from '../redux/auth/authSlice'
@@ -8,7 +9,12 @@ import { getAllUsers } from '../redux/users/usersSlice'
 
 export const PeoplePage = () => {
   const isAuth = useSelector(checkIsAuth)
+  const user = useSelector((state)=> state.auth.user)
   const users = useSelector((state) => state.users.users)
+  const [isOpen, setIsOpen] = useState(false)
+  const [activeImage, setActiveImage] = useState('')
+  const [activeName, setActiveName] = useState('')
+  const [activeBirthdate, setActiveBirthdate] = useState('')
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
@@ -16,18 +22,34 @@ export const PeoplePage = () => {
     if (!isAuth) navigate('/')
     dispatch(getAllUsers())
   }, [isAuth, navigate, dispatch])
+
+  const filteredUsers = users.filter((u)=>u._id !== user._id)
+
+  if(!filteredUsers) return <div className='text-lg text-gray-800'>Список пользователей пуст</div>
+
   return (
     <div>
       <Navbar />
       <div
         className='flex justify-center mt-10 gap-10 flex-wrap'>
-        {users?.map((e) => <UserItem
+        {filteredUsers?.map((e) => <UserItem
           key={e._id}
           username={e.username}
           userPhoto={e.userPhoto}
+          birthdate={e.birthdate}
+          setIsOpen={setIsOpen}
+          setActiveName={setActiveName}
+          setActiveBirthdate={setActiveBirthdate}
+          setActiveImage={setActiveImage}
           id={e._id}>
         </UserItem>)}
       </div>
+      <Info 
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        activeName={activeName}
+        activeBirthdate={activeBirthdate}
+        activeImage={activeImage}/>
     </div>
   )
 }
